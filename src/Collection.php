5960508a599bcda097b599e62a0d36e9fb7a0245
David Kyalo <davidmkyalo@gmail.com>
2017-01-02 00:58:07 +0300

    [ALT] Changed the update() method to only use array_replace_* then dropped overriding the merge() method and adopted new replace() method instead.

--- src/Collection.php
+++ src/Collection.php
@@ -92,21 +92,20 @@ class Collection extends IlluminateCollection implements Arrayable, Jsonable
 	}
 
 	/**
-	 * Get a new collection with the items in the current merged with the given
+	 * Get a new collection with the items in the current replaced with the given
 	 * items. This method is identical to {@see \Tea\Collections\Collection::update()}
-	 * except it creates a new instance with the merged items leaving the current
+	 * except it returns a new instance with the replaced items leaving the current
 	 * untouched.
 	 *
-	 * @see   \Tea\Collections\Collection::update()
+	 * @uses   \Tea\Collections\Collection::update()
 	 *
 	 * @param  mixed  $items
-	 * @param  bool   $strict
 	 * @param  bool   $recursive
 	 * @return static
 	 */
-	public function merge($items, $strict = false, $recursive = false)
+	public function replace($items, $recursive = false)
 	{
-		return $this->copy()->update($items, $strict, $recursive);
+		return $this->copy()->update($items, $recursive);
 	}
 
 	/**
@@ -115,41 +114,22 @@ class Collection extends IlluminateCollection implements Arrayable, Jsonable
 	 * the value in the new $items. If an item exists in the new $items but not
 	 * in the collection, it will be added to the collection as well.
 	 *
-	 * By default, only the items with string keys are updated. Those of numeric
-	 * keys, are instead appended to the collection with (possibly) new key(s)
-	 * even if the original collection contained item(s) with the same key.
-	 * However, if $strict is passed as true, numerically indexed items will also
-	 * be updated.
-	 *
-	 * Also, {@see \Tea\Collections\Collection::merge()}.
-	 *
-	 * When $strict is false or not given:
-	 * @uses   array_merge() when $recursive is false (default)
-	 * @uses   array_merge_recursive() when $recursive is true
-	 *
-	 * When $strict is true:
-	 * @uses   array_replace() when $recursive is false (default)
-	 * @uses   array_replace_recursive() when $recursive is true
+	 * @see    \Tea\Collections\Collection::replace()
+	 * @uses   array_replace() when $recursive is false (default).
+	 * @uses   array_replace_recursive() when $recursive is true.
 	 *
 	 * @param  mixed  $items
-	 * @param  bool   $strict
 	 * @param  bool   $recursive
 	 * @return $this
 	 */
-	public function update($items, $strict = false, $recursive = false)
+	public function update($items, $recursive = false)
 	{
 		$items = $this->getArrayableItems($items);
 
-		if($strict)
-			if($recursive)
-				$this->items = array_replace_recursive($this->items, $items);
-			else
-				$this->items = array_replace($this->items, $items);
+		if($recursive)
+			$this->items = array_replace_recursive($this->items, $items);
 		else
-			if($recursive)
-				$this->items = array_merge_recursive($this->items, $items);
-			else
-				$this->items = array_merge($this->items, $items);
+			$this->items = array_replace($this->items, $items);
 
 		return $this;
 	}
